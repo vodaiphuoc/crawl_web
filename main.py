e41a35e3ea71d39996fd4f191da4d7a6cbbb2f3a
vodaiphuoc <phuocvodn98gmail.com>
2025-03-18 07:06:16 +0000

    oke

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import json
 import glob
 from model.inference import Inference
+from bs4 import BeautifulSoup
+
+def pre_processing_page_data(page_data:str)->str:
+    soup = BeautifulSoup(page_data, 'html.parser')
+
+    title = soup.find_all("h1", attrs= {"class": "title", "data-role": "title"})[0].text
+    title = title.strip()
+    
+    lines = soup.text.strip().split('\n')
+    lines = [line.strip() for line in lines if line != '']
+
+    title_idx = [ith for ith, line in enumerate(lines) if line == title][-1]
+
+    end_corpus_idx = [ith for ith, line in enumerate(lines) if line == 'Lấy link!'][-1]
+
+    main_corpus = "\n".join(lines[title_idx+1:end_corpus_idx-4])
+
+    return main_corpus
+
 
 model = Inference(model_id_list = "google/gemma-3-1b-it")
 BATCH_SIZE = 3
@@ -18,7 +37,10 @@ for json_file in glob.glob('stage_3_data/*.json'):
             if ith + BATCH_SIZE < len(stage3_data)  \
             else stage3_data[ith: ith : ]
 
-        batch_page = [page['page_data'] for page in batch_pages_data]
+        batch_page = [
+            pre_processing_page_data(page['page_data']) 
+            for page in batch_pages_data
+        ]
 
         batch_reponses = model.forward(batch_page)
 
