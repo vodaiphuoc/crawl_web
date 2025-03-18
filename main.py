21468e60e517f68c51e8227e320ca4e0f750152b
vodaiphuoc <phuocvodn98gmail.com>
2025-03-18 10:10:58 +0000

    add arg

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import glob
 from model.inference import Inference
 from bs4 import BeautifulSoup
+import argparse
 
 def pre_processing_page_data(page_data:str)->str:
     soup = BeautifulSoup(page_data, 'html.parser')
@@ -21,49 +22,59 @@ def pre_processing_page_data(page_data:str)->str:
     return main_corpus
 
 
-model = Inference(model_id_list = "google/gemma-3-1b-it")
-BATCH_SIZE = 3
+def main(batch_size:int):
+
+    model = Inference(model_id_list = "google/gemma-3-1b-it")
 
 
-total_data = []
-_ith = 0
+    total_data = []
+    _ith = 0
 
-for json_file in glob.glob('stage_3_data/*.json'):
-    with open(json_file,'r') as fp:
-        stage3_data = json.load(fp)
+    for json_file in glob.glob('stage_3_data/*.json'):
+        with open(json_file,'r') as fp:
+            stage3_data = json.load(fp)
 
-    for ith in range(0,len(stage3_data), BATCH_SIZE):
-        print('batch start id: ', ith)
+        for ith in range(0,len(stage3_data), batch_size):
+            print('batch start id: ', ith)
 
-        batch_pages_data = stage3_data[ith: ith + BATCH_SIZE] \
-            if ith + BATCH_SIZE < len(stage3_data)  \
-            else stage3_data[ith: ith : ]
+            batch_pages_data = stage3_data[ith: ith + batch_size] \
+                if ith + batch_size < len(stage3_data)  \
+                else stage3_data[ith: ith : ]
 
 
-        batch_page = []
-        for page in batch_pages_data:
-            try:
-                batch_page.append(pre_processing_page_data(page['page_data']))
-            except IndexError as err:
-                continue
-        
-        batch_reponses = model.forward(batch_page)
-
-        output_batch = [{
-                "key": origin_data["key"],
-                "url": origin_data["url"],
-                "page_data": origin_data["page_data"],
-                "NER": reponse_data
-            }
-            for origin_data, reponse_data \
-            in zip(batch_pages_data, batch_reponses)
-        ]
-
-        total_data.extend(output_batch)
-
-        if len(total_data) > 100:
-            _ith += 1
-            with open(f'stage_4_data/page_data_{_ith}.json','w') as fp:
-                json.dump(total_data, fp, indent= 4)
+            batch_page = []
+            for page in batch_pages_data:
+                try:
+                    batch_page.append(pre_processing_page_data(page['page_data']))
+                except IndexError as err:
+                    continue
             
-            total_data = []
+            batch_reponses = model.forward(batch_page)
+
+            output_batch = [{
+                    "key": origin_data["key"],
+                    "url": origin_data["url"],
+                    "page_data": origin_data["page_data"],
+                    "NER": reponse_data
+                }
+                for origin_data, reponse_data \
+                in zip(batch_pages_data, batch_reponses)
+            ]
+
+            total_data.extend(output_batch)
+
+            if len(total_data) > 100:
+                _ith += 1
+                with open(f'stage_4_data/page_data_{_ith}.json','w') as fp:
+                    json.dump(total_data, fp, indent= 4)
+                
+                total_data = []
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size")
+    args = vars(parser.parse_args())
+
+    main(**args)
+    
